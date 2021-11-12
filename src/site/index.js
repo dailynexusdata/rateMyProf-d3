348003ebfae79032496d9184264bbb695a2f79f6
zachgoldy <bloonsmstr54@gmail.com>
2021-11-12 13:19:29 -0800

    set up template with data

--- src/site/index.js
+++ src/site/index.js
@@ -3,19 +3,20 @@
  * @since 2021-11-12
  */
 import './styles.scss';
-
 import { csv, json } from 'd3-fetch';
+import makeBarPlots from './plots/barplots';
+import makeLineCharts from './plots/linecharts';
 
 // import plot functions here:
 // import makePLOT_NAME from "./PLOT_NAME";
 
 const main = async () => {
-  // import data - use csv or json:
-  // const data = await csv('file path or url');
+  const linesData = await d3.csv('dist/data/lines.csv');
+  const barplotsData = await d3.csv('dist/data/barplots.csv');
 
   const resize = () => {
-    // call imported plots here:
-    // makePLOT_NAME(data);
+    makeBarPlots(barplotsData);
+    makeLineCharts(linesData);
   };
 
   window.addEventListener('resize', () => {
---
+++ src/site/plots/linecharts.js
@@ -0,0 +1,69 @@
+/**
+ * Line Charts
+ *
+ * @author Alec Chen
+ *
+ */
+import { select } from 'd3-selection';
+import { scaleLinear } from 'd3-scale';
+
+/**
+ * @param {*} data - What is the data?
+ *
+ * @author Name
+ *
+ * @since Date
+ */
+const makeLineCharts = (data) => {
+  /*
+    Container Setup:
+  */
+
+  // The class is necessary to apply styling
+  const container = select('#rate-my-prof-div-id').attr('class', 'rate-my-prof');
+
+  // When the resize event is called, reset the plot
+  container.selectAll('*').remove();
+
+  container.append('h1').text('My title');
+
+  const size = {
+    height: 400,
+    width: Math.min(600, window.innerWidth - 40),
+  };
+
+  const margin = {
+    top: 10,
+    right: 10,
+    bottom: 10,
+    left: 10,
+  };
+
+  const svg = container
+    .append('svg')
+    .attr('height', size.height)
+    .attr('width', size.width);
+
+  container
+    .append('a')
+    .text('Source: __________')
+    .attr('href', '');
+
+  /*
+    Create Scales:
+  */
+
+  const x = scaleLinear()
+    .domain([0, 1])
+    .range([margin.left, size.width - margin.right]);
+
+  const y = scaleLinear()
+    .domain([0, 1])
+    .range([size.height - margin.bottom, margin.top]);
+
+  /*
+    Start Plot:
+  */
+};
+
+export default makeLineCharts;
