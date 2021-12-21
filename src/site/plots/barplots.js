f7643677b86635deb9f1cd50bd63569eface2555
Jordan Russo <jrusso0818@gmail.com>
2021-12-21 13:30:17 -0800

    Started Bar Plot

--- src/site/plots/barplots.js
+++ src/site/plots/barplots.js
@@ -5,14 +5,14 @@
  *
  */
 import { select } from 'd3-selection';
-import { scaleLinear } from 'd3-scale';
+import { scaleBand, scaleLinear } from 'd3-scale';
 
 /**
  * @param {*} data - What is the data?
  *
- * @author Name
+ * @author Jordan Russo
  *
- * @since Date
+ * @since 12/21/2021
  */
 const makeBarPlots = (data) => {
   /*
@@ -20,12 +20,14 @@ const makeBarPlots = (data) => {
   */
 
   // The class is necessary to apply styling
-  const container = select('#rate-my-prof-bar-id').attr('class', 'rate-my-prof');
+  console.log(data);
+  const container = select('#rate-my-prof-bar-plot')
+    .attr('class', 'rate-my-prof');
 
   // When the resize event is called, reset the plot
   container.selectAll('*').remove();
 
-  container.append('h1').text('My title');
+  container.append('h1').text('My title2');
   const size = {
     height: 400,
     width: Math.min(600, window.innerWidth - 40),
@@ -52,17 +54,29 @@ const makeBarPlots = (data) => {
     Create Scales:
   */
 
-  const x = scaleLinear()
-    .domain([0, 1])
-    .range([margin.left, size.width - margin.right]);
+  const x = scaleBand()
+    .domain(data.map((dataPoint) => dataPoint.word))
+    .range([margin.left, size.width - margin.right])
+    .paddingInner(0.1);
 
   const y = scaleLinear()
-    .domain([0, 1])
+    .domain([0, 11000])
     .range([size.height - margin.bottom, margin.top]);
 
   /*
     Start Plot:
   */
+  const bars = svg
+    .selectAll('.bar')
+    .data(data)
+    .enter()
+    .append('rect')
+    .classed('bar', true)
+    .attr('width', x.bandwidth())
+    .attr('height', (data) => size.height - y(data.n))
+    .attr('x', (data) => x(data.word))
+    .attr('y', (data) => y(data.n))
+    .style('fill', '#69b3a2');
 };
 
 export default makeBarPlots;
