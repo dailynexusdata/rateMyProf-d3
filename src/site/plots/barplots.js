004eb59d740051e28cf37f77cbf08c43995ad727
Jordan Russo <jrusso0818@gmail.com>
2021-12-24 16:46:58 -0800

    Barplot changed format

--- src/site/plots/barplots.js
+++ src/site/plots/barplots.js
@@ -37,15 +37,18 @@ const makeBarPlots = (data) => {
 
   const margin = {
     top: 10,
-    right: 10,
     bottom: 20,
     left: 10,
+    midright: (size.width / 2) - 50,
+    midleft: (size.width / 2) + 50,
+    right: 10,
   };
 
   const svg = container
     .append('svg')
     .attr('height', size.height)
-    .attr('width', size.width);
+    .attr('width', size.width)
+    .style('background-color', '#ededed');
 
   container
     .append('a')
@@ -70,7 +73,7 @@ const makeBarPlots = (data) => {
 
   const x = scaleLinear()
     .domain([0, 11000])
-    .range([margin.left, (size.width / 2)]);
+    .range([margin.left, margin.midright]);
 
   const posY = scaleBand()
     .domain(positiveData.map((dataPoint) => dataPoint.word))
@@ -104,10 +107,11 @@ const makeBarPlots = (data) => {
     .on('mouseenter', (event, d) => {
       svg
         .append('text')
-        .attr('x', x(d.n) / 2)
+        .attr('x', margin.left + x(d.n))
         .attr('y', posY(d.word) + 20)
-        .text(`n = ${d.n}`)
-        .attr('class', 'hover-over-text');
+        .text(d.n)
+        .attr('class', 'hover-over-text')
+        .style('font-size', 'small');
     })
     .on('mouseleave', () => {
       selectAll('.hover-over-text').remove();
@@ -119,9 +123,11 @@ const makeBarPlots = (data) => {
     .enter()
     .append('text')
     .text((d) => d.word)
-    .attr('x', margin.left)
+    .attr('x', 0)
     .attr('y', (d) => posY(d.word) + 20)
-    .attr('class', 'positive-text');
+    .attr('class', 'positive-text')
+    .style('margin', 'auto')
+    .style('font-size', 'small');
 
   const negBars = svg
     .selectAll('.neg-bar')
@@ -131,9 +137,21 @@ const makeBarPlots = (data) => {
     .classed('neg-bar', true)
     .attr('width', (data) => x(data.n))
     .attr('height', negY.bandwidth())
-    .attr('x', (size.width / 2) + 10)
+    .attr('x', margin.midleft)
     .attr('y', (data) => negY(data.word))
-    .style('fill', colors.red);
+    .style('fill', colors.red)
+    .on('mouseenter', (event, d) => {
+      svg
+        .append('text')
+        .attr('x', margin.midleft)
+        .attr('y', negY(d.word) + 20)
+        .text(d.n)
+        .attr('class', 'hover-over-text')
+        .style('font-size', 'small');
+    })
+    .on('mouseleave', () => {
+      selectAll('.hover-over-text').remove();
+    });
 
   const negText = svg
     .selectAll('labels')
@@ -141,23 +159,26 @@ const makeBarPlots = (data) => {
     .enter()
     .append('text')
     .text((d) => d.word)
-    .attr('x', (size.width / 2) + 10)
-    .attr('y', (d) => negY(d.word) + 20);
+    .attr('x', margin.midleft)
+    .attr('y', (d) => negY(d.word) + 20)
+    .attr('class', 'negative-text')
+    .style('font-size', 'small');
 
+  // postive axis
   svg
     .append('g')
-    .attr('transform', `translate(0, ${size.height - margin.bottom})`)
-    .attr('color', '#adadad')
+    .attr('transform', `translate(${margin.left}, ${size.height - margin.bottom})`)
+    .attr('color', 'black')
     .call(
       axisBottom(x)
-        .ticks(4)
-        .tickFormat((d) => d),
+        .ticks(4),
     );
 
+  // negative axis
   svg
     .append('g')
-    .attr('transform', `translate(${size.width / 2}, ${size.height - margin.bottom})`)
-    .attr('color', '#adadad')
+    .attr('transform', `translate(${margin.midleft}, ${size.height - margin.bottom})`)
+    .attr('color', 'black')
     .call(
       axisBottom(x)
         .ticks(4)
